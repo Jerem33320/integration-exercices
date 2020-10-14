8af8ffd01262cc87ee1c5ba1eb10345071bb92ea
Jeremy Peyrat <prstoad@gmail.com>
2020-10-14 15:57:13 +0200

    add multiple days section 2

--- meteo/app.js
+++ meteo/app.js
@@ -64,7 +64,7 @@ const app = new function(){
     fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&lang=fr&APPID=a53af32d6d0eefb218dd3478d2375a5c')
     .then(res => res.json())
     .then(data => {
-      console.log(data);
+      // console.log(data);
       // ----HEADER
       this.getBackgroundImg(data);
       this.city.innerHTML = data.name;
@@ -86,7 +86,7 @@ const app = new function(){
     .catch(err => console.log('Request Failed', err));
   }
 
-  // this.getFuturWeather = function(){
+  this.getFuturWeather = function(){
     fetch('http://api.openweathermap.org/data/2.5/forecast?q=London,uk&units=metric&lang=fr&APPID=a53af32d6d0eefb218dd3478d2375a5c')
     .then(res => res.json())
     .then(data => {
@@ -96,13 +96,17 @@ const app = new function(){
         // this.weatherDiv.append(data.list[i].weather[0].description);
         // const actualIcon = document.createElement('img');
         // actualIcon.src = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
+        const newWeatherDiv = document.createElement('div');
+        newWeatherDiv.classList.add('weather-sec-section');
         let futurDays = this.createSecSectionDiv(data.list[i].dt_txt, "Icon", Math.floor(data.list[i].main.temp));
+        newWeatherDiv.innerHTML = futurDays;
         console.log('futur: ' + futurDays);
-        this.weatherDiv.append(futurDays);
+
+        this.secSection.appendChild(newWeatherDiv);
       }
     })
     .catch(err => console.log('Request Failed', err));
-  
+  }
 
   this.createSecSectionDiv = function(day, icon, temp){
     return `
