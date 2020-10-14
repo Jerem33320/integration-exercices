25b31c99865a8ac8172e0793bd731d3b0dbe32bc
Jeremy Peyrat <prstoad@gmail.com>
2020-10-14 16:14:36 +0200

    add icons to sec section

--- meteo/app.js
+++ meteo/app.js
@@ -93,15 +93,13 @@ const app = new function(){
       console.log(data);
       for(let i = 0; i < data.list.length; i++){
         console.log(data.list[i]);
-        // this.weatherDiv.append(data.list[i].weather[0].description);
-        // const actualIcon = document.createElement('img');
-        // actualIcon.src = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
+
+        let futurIcon = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
+
         const newWeatherDiv = document.createElement('div');
         newWeatherDiv.classList.add('weather-sec-section');
-        let futurDays = this.createSecSectionDiv(data.list[i].dt_txt, "Icon", Math.floor(data.list[i].main.temp));
+        let futurDays = this.createSecSectionDiv(data.list[i].dt_txt, futurIcon, Math.floor(data.list[i].main.temp) + '°');
         newWeatherDiv.innerHTML = futurDays;
-        console.log('futur: ' + futurDays);
-
         this.secSection.appendChild(newWeatherDiv);
       }
     })
@@ -110,11 +108,11 @@ const app = new function(){
 
   this.createSecSectionDiv = function(day, icon, temp){
     return `
-    <div class="weather-sec-section">
       <div>${day}</div>
-      <div id="actual-weather-icon">${icon}</div>
+      <div id="actual-weather-icon">
+        <img src="${icon}" alt="img-weather">
+      </div>
       <div id="secSection-temp">${temp}</div>
-    </div>
     `;
   }
 }
